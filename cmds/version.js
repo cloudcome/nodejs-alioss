ad949ebd2a524d0c26c5d0b3ab8c9425a34ff66c
cloudcome <cloudcome@163.com>
2015-12-14 00:35:43 +0800

    alioss

--- cmds/version.js
+++ cmds/version.js
@@ -7,7 +7,26 @@
 
 'use strict';
 
-var fs = require('fs');
+
+var npm = require('ydr-utils').npm;
+var debug = require('ydr-utils').debug;
+
+var pkg = require('../package.json');
+
+module.exports = function () {
+    debug.success('local version', pkg.version);
+    debug.ignore('check version', 'wait a moment...');
+    npm.getLatestVersion(pkg.name, function (err, version, json) {
+        if (err) {
+            debug.error('online version', err.message);
+            return process.exit(1);
+        }
+
+        debug.success('online version', version);
+    });
+};
+
+
 
 
 
