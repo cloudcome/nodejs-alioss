f806302120a946d703970a0d8bafbae5d2278493
cloudcome <cloudcome@163.com>
2015-12-14 00:07:33 +0800

    alioss

--- cmds/clear.js
+++ cmds/clear.js
@@ -7,7 +7,23 @@
 
 'use strict';
 
-var fs = require('fs');
+var fse = require('fs-extra');
+var path = require('ydr-utils').path;
+var debug = require('ydr-utils').debug;
+
+module.exports = function (options) {
+    var destCacheFile = path.join(options.srcDirname, '7niu.cache.log');
+
+    try {
+        fse.removeSync(destCacheFile);
+        debug.success('clear cache', 'success');
+    } catch (err) {
+        debug.error('clear cache', path.toSystem(destCacheFile));
+        debug.error('clear cache', err.message);
+        return process.exit(1);
+    }
+};
+
 
 
 
